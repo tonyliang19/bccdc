03509aee4a4aa30568a5e6403631be3691179719
jh22d <jh0220x@gmail.com>
2023-10-08 14:25:09 -0700

    update var_assign description

--- R/var_assign.R
+++ R/var_assign.R
@@ -1,4 +1,7 @@
-#' Title Select sheet, columns and diseases for Coverage&Reasons analysis
+#' Variable Assignment
+#'
+#' @description Select sheet, columns and diseases for Coverage & Reasons
+#' analysis per age/grade group
 #'
 #' @param group_name A string selected from
 #' c("7yr","6gr","7gr","8gr","9gr","10gr","11gr","12gr")
@@ -10,11 +13,15 @@
 #'  related to Coverage among all ages&grades in the dataset
 #'
 #' @return A list of assigned variables in length of 4, including
+#'
 #' group_sheet, cnames, Disease_coverage and Disease_reason as per group
 #'
 #' group_sheet: String of the sheet name in the dataset to be analyzed
+#'
 #' cnames: Vector of the codable column names in selected sheet
+#'
 #' Disease_coverage: Vector of readable diseases&variants&notes to be listed in the Coverage file
+#'
 #' Disease_reason: Vector of readable diseases to be listed in the NonImmReason file
 #'
 #' @export
