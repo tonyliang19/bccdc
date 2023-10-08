ba4e435700f9a2356e1cd0ccc91a4b033a739cd1
jh22d <jh0220x@gmail.com>
2023-10-08 15:00:47 -0700

    add roxygen selecken to atg_ex

--- R/atg_ex.R
+++ R/atg_ex.R
@@ -1,3 +1,38 @@
+#' Target group and filter out excluded antigen
+#'
+#' @description Select correct grade / year level & HA / HSDA
+#' and exclude unnecessary antigen
+#'
+#' @param df Original dataset
+#' @param HA Boolean, indicating whether used for HA analysis or HSDA analysis
+#' @param atg_ex List of all excluded antigen in the original dataset
+#' @param group_name String of group name
+#' @param grade_label String of grade in 2 digit converted from group_name
+#'
+#' @return Dataset with only correct antigen and HA / HSDA of target group
+#'
+#' @export
+#'
+#' @examples
+#' df_g <- data.frame(
+#' grade = c("06","06","07","08", "09", "10", "10", "11", "12"),
+#' antigen = c("dtap", "dtap_p", "hpv_F", "hpv_M", "men", "hpv", "measle",
+#' "td", "poilo"),
+#' HSDA = c("Vancouver", "Richmond", "VCHA", "Richmond", "Vancouver",
+#' "VCHA", "VCHA", "Vancouver", "Richmond")
+#' )
+#'
+#' df_y<- data.frame(
+#' antigen = c("dtap", "dtap_p", "hpv_F", "hpv_M", "men", "hpv", "measle",
+#' "td", "poilo"),
+#' HSDA = c("Vancouver", "Richmond", "VCHA", "Richmond", "Vancouver",
+#' "VCHA", "VCHA", "Vancouver", "Richmond")
+#' )
+#'
+#' atg_ex(df_y, TRUE, c("dtap", "dtap_p", "hpv_F", "hpv_M"), "7yr")
+#' atg_ex(df_g, TRUE, c("dtap", "dtap_p", "hpv_F", "hpv_M"), "7gr","07")
+#' atg_ex(df_g, FALSE, "td","12gr","12")
+#'
 atg_ex <- function(df, HA = TRUE, atg_ex, group_name, grade_label = ""){
 
   if (!is.data.frame(df)) {
