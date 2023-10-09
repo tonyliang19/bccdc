5211ad78ca25c18fa011f8bc5e9031bba794586b
jh22d <jh0220x@gmail.com>
2023-10-08 20:02:52 -0700

    add roxygen skeleton to atg_in

--- R/atg_in.R
+++ R/atg_in.R
@@ -1,3 +1,37 @@
+#'Target group and keep included antigen
+#'
+#' @description Select correct grade / year level & HA / HSDA
+#' and include unnecessary antigen
+#'
+#' @param df Original Dataset
+#' @param HA Boolean, indicating whether used for HA analysis of HSDA analysis
+#' @param atg_in List of all included antigen in the original dataset
+#' @param group_name String of group name
+#' @param grade_label String of grade in 2 digits converted from group_name
+#'
+#' @return Dataset with only correct antigen and HA / HSDA of target group
+#' @export
+#'
+#' @examples
+#' df_g <- data.frame(
+#' grade = c("06","06","07","08", "09", "10", "10", "11", "12"),
+#' antigen = c("dtap", "dtap_p", "hpv_F", "hpv_M", "men", "hpv", "measle",
+#' "td", "poilo"),
+#' HSDA = c("Vancouver", "Richmond", "VCHA", "Richmond", "Vancouver",
+#' "VCHA", "VCHA", "Vancouver", "Richmond")
+#' )
+#'
+#' df_y<- data.frame(
+#' antigen = c("dtap", "dtap_p", "hpv_F", "hpv_M", "men", "hpv", "measle",
+#' "td", "poilo"),
+#' HSDA = c("Vancouver", "Richmond", "VCHA", "Richmond", "Vancouver",
+#' "VCHA", "VCHA", "Vancouver", "Richmond")
+#' )
+#'
+#' atg_in(df_y, TRUE, c("dtap", "dtap_p", "hpv_F", "hpv_M"), "7yr")
+#' atg_in(df_g, TRUE, c("dtap", "dtap_p", "hpv_F", "hpv_M"), "7gr","07")
+#' atg_in(df_g, FALSE, "td","12gr","12")
+
 atg_in <- function(df, HA = TRUE, atg_in, group_name, grade_label = ""){
 
   gradeage = substr(group_name,
