97fc33ce0340a52ef677c67c0bab1278d6512c30
jh22d <jh0220x@gmail.com>
2023-10-08 21:33:20 -0700

    add function tran_cvg defination

--- R/trans_cvg.R
+++ R/trans_cvg.R
@@ -1,3 +1,51 @@
+#' Dataset Preparation
+#'
+#' @description Initial manipulate the dataset as per group for Coverage analysis
+#' @param group_name A string selected from c("7yr","6gr","7gr","8gr","9gr",
+#' "10gr","11gr","12gr")
+#' @param grade_label String of grade in 2 digits converted from group_name
+#' @param df Original dataset
+#'
+#' @return A list in length 9, including
+#'
+#' vch_df_ha: dataset for HA Coverage analysis
+#'
+#' vch_df_hsda: dataset for HDSA Coverage analysis
+#'
+#' disease_tbmutate: string of a disease to be deeper explored
+#'
+#' afternrows: number of the row to inserted the mutated disease
+#'
+#' disease_rename: string of the new name of the variant disease
+#'
+#' vch_hpv_ha: dataset for HA HPV related analysis
+#'
+#' vch_hpv_hsda: dataset for HSDA related analysis
+#'
+#' reason_df_ha: dataset for HA reason analysis
+#'
+#' reason_df_hsda: dataset for HSDA reason analysis
+#'
+#' @export
+#' @examples
+#' vch_df0 <- data.frame(
+#' antigen = c("dtap", "dtap_p", "hpv_F", "hpv_M", "men", "hpv", "measle",
+#' "td", "poilo"),
+#' HSDA = c("Vancouver", "Richmond", "VCHA", "Richmond", "Vancouver",
+#' "VCHA", "VCHA", "Vancouver", "Richmond")
+#' )
+#' trans_cvg("7yr")
+#'
+#' vch_df_g <- data.frame(
+#' grade = c("06","06","07","08", "09", "10", "10", "11", "12"),
+#' antigen = c("dtap", "dtap_p", "hpv_F", "hpv_M", "men", "hpv", "measle",
+#' "td", "poilo"),
+#' HSDA = c("Vancouver", "Richmond", "VCHA", "Richmond", "Vancouver",
+#' "VCHA", "VCHA", "Vancouver", "Richmond")
+#' )
+#' trans_cvg("6gr", "06", vch_df_g)
+#'
+#'
 trans_cvg <- function(group_name, grade_label = "", df = vch_df0){
   HPV_dis = c("hpv_M", "hpv_F")
 
